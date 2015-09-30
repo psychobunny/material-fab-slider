bb71944abb80626139ce4945149ef430560a2d13
psychobunny <rodrigues.andrew@gmail.com>
2015-09-30 17:22:06 -0400

    slider sliding!

--- public/lib/main.js
+++ public/lib/main.js
@@ -7,7 +7,11 @@ $(document).ready(function() {
 		mfs.init();
 	});
 
-	$('.slider').on('mouseup', function() {
+	$('.slider').on('touchstart', function() {
+
+	});
+
+	$('.slider').on('touchend', function() {
 		$('.material-fab-slider')
 			.removeClass('animated')
 			.addClass('animated-backwards');
@@ -17,6 +21,40 @@ $(document).ready(function() {
 			})
 	});
 });
+
+var dragging = false, originalX;
+
+$('.slider').on('touchstart', function(ev) {
+	dragging = true;
+	originalX = ev.originalEvent.touches[0].clientX;
+	$('.material-fab-slider').addClass('dragging');
+	$('.slider').addClass('active');
+});
+
+$('body').on('touchend', function() {
+	if (dragging === true) {
+		dragging = false;
+		$('.material-fab-slider').removeClass('dragging');
+		$('.slider').removeClass('active');
+	}
+});
+
+$('body').on('touchmove', function(ev) {
+	if (!dragging) {
+		return;
+	}
+
+	var percentage = (ev.originalEvent.touches[0].clientX - $('.progress-bar').offset().left - $('.handle').width() / 2) / $('.progress-bar').width() * 100;
+	$('.progress-bar-empty')
+		.css({
+			width: 100 - percentage + '%'
+		});
+
+	$('.slider').css({
+			left: $('.progress-bar-empty').offset().left - $('.slider').width() / 2
+		});
+});
+
 
 
 var mfs = {};
