3ca74f4143f90310a586dda00b9db5b21960d09a
psychobunny <rodrigues.andrew@gmail.com>
2015-09-30 15:31:26 -0400

    slider intro

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,7 +1,33 @@
+"use strict";
+
 var $ = require('jquery');
 
 $(document).ready(function() {
 	setTimeout(function() {
 		$('body').addClass('animated');
+		mfs.init(50);
 	}, 500);
-})
+});
+
+
+var mfs = {};
+
+mfs.init = function(initialPercentage) {
+	initialPercentage = initialPercentage || 50;
+
+	setTimeout(function() {
+		mfs.set(initialPercentage);
+	}, 500);
+};
+
+mfs.set = function(percentage, done) {
+	$('.progress-bar-empty')
+		.css({
+			width: percentage + '%'
+		})
+		.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function() {
+			$('.slider').css({
+				left: $('.progress-bar-empty').offset().left - $('.slider').width() / 2
+			});
+		});
+};
