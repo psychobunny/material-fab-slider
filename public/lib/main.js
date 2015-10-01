0299cfada741da256b334fe793af0ae279588e71
psychobunny <rodrigues.andrew@gmail.com>
2015-10-01 15:34:52 -0400

    cleanup

--- public/lib/main.js
+++ public/lib/main.js
@@ -1,49 +1,30 @@
 "use strict";
 
 var $ = require('jquery');
+var currentPercentage;
 
 $(document).ready(function() {
 	$('.fab').on('click', function() {
-		mfs.init(currentPercentage);
-	});
-
-	$('.slider').on('touchstart', function() {
-
-	});
-
-	$('.slider').on('touchend', function() {
-		$('.material-fab-slider')
-			.removeClass('animated')
-			.addClass('animated-backwards');
-		$('.progress-bar-empty')
-			.css({
-				width: '0%'
-			})
+		mfs.show(currentPercentage);
 	});
 });
 
-var dragging = false, originalX, currentPercentage;
-
 $('.slider').on('touchstart', function(ev) {
-	dragging = true;
-	originalX = ev.originalEvent.touches[0].clientX;
 	$('.material-fab-slider').addClass('dragging');
 	$('.slider').addClass('active');
+	$('body').on('touchmove', onTouchMove);
 });
 
 $('body').on('touchend', function() {
-	if (dragging === true) {
-		dragging = false;
+	if ($('.material-fab-slider').hasClass('dragging')) {
 		$('.material-fab-slider').removeClass('dragging');
 		$('.slider').removeClass('active');
+		$('body').off('touchmove', onTouchMove);
+		mfs.hide();
 	}
 });
 
-$('body').on('touchmove', function(ev) {
-	if (!dragging) {
-		return;
-	}
-
+function onTouchMove(ev) {
 	var percentage = 100 - (ev.originalEvent.touches[0].clientX - $('.progress-bar').offset().left - $('.handle').width() / 2) / $('.progress-bar').width() * 100;
 	$('.progress-bar-empty')
 		.css({
@@ -57,23 +38,19 @@ $('body').on('touchmove', function(ev) {
 	currentPercentage = percentage;
 
 	$('.indicator').html(parseInt(percentage, 10));
-});
+}
 
 
 
 var mfs = {};
 
-mfs.init = function(initialPercentage) {
-	initialPercentage = initialPercentage || 50;
+mfs.show = function(percentage) {
+	percentage = percentage || 50;
 
 	$('.material-fab-slider')
 		.removeClass('animated-backwards')
 		.addClass('animated');
 
-	mfs.set(initialPercentage);
-};
-
-mfs.set = function(percentage, done) {
 	$('.progress-bar-empty')
 		.css({
 			width: percentage + '%'
@@ -86,3 +63,13 @@ mfs.set = function(percentage, done) {
 			if (typeof done === 'function') { done(); }
 		});
 };
+
+mfs.hide = function() {
+	$('.material-fab-slider')
+		.removeClass('animated')
+		.addClass('animated-backwards');
+	$('.progress-bar-empty')
+		.css({
+			width: '0%'
+		});
+}
